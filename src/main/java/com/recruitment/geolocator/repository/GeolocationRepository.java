8f5ca557368081d045f03deeb4be52ab6093c954
MajaBrog <majaa.brog@gmail.com>
2021-10-28 02:00:54 +0200

    Add Aspect for logging execution of service, controller, and repository

--- src/main/java/com/recruitment/geolocator/repository/GeolocationRepository.java
+++ src/main/java/com/recruitment/geolocator/repository/GeolocationRepository.java
@@ -2,10 +2,11 @@ package com.recruitment.geolocator.repository;
 
 import com.recruitment.geolocator.domain.Geolocation;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
+@Repository
 public interface GeolocationRepository extends CrudRepository<Geolocation, Long> {
     @Override
     List<Geolocation> findAll();
